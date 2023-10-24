59af1d1e5cff2f570662201381f5764f24a23199
chahasully <148852561+chahasully@users.noreply.github.com>
2023-10-24 21:20:49 +1300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+basic.show_number(24)
+
+def on_forever():
+    basic.show_number(23)
+    basic.show_number(22)
+    basic.show_number(21)
+    basic.show_number(20)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showNumber(24)
+basic.forever(function on_forever() {
+    basic.showNumber(23)
+    basic.showNumber(22)
+    basic.showNumber(21)
+    basic.showNumber(20)
+})
